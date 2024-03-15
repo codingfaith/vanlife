85084a36554a3c46abba3b90c51e1233f017017d
codingfaith <codingfaith158@gmail.com>
2024-03-15 14:38:31 +0200

    Populate about page

--- components/About.jsx
+++ components/About.jsx
@@ -7,6 +7,18 @@ export default function About(){
         <div className="container">
             <Nav />
             <img src="../img/aboutbg.png" alt="van image" className="about-image"/>
+            <div className="about-body">
+                <h3>Don't squeeze in a sedan when you could relax in a van.</h3><br />
+                <p>Our mission is to enliven your roadtrip with the perfect travel van rental. Our vans are recertified before each
+                    trip to ensure your travel plans can go off without a hitch. (Hitch costs extra😉)
+                </p><br />
+                <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p><br />
+                <div className="explore">
+                    <h4>Your destination is waiting.</h4>
+                    <h4>Your van is ready.</h4><br />
+                    <button className="explore-button">Explore our vans</button>
+                </div>
+            </div>
             <Footer />
         </div>
     )

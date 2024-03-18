6484b34df41f8176f685a2c2caac4b5f3756692d
codingfaith <codingfaith158@gmail.com>
2024-03-18 16:22:42 +0200

    Style layout of van elements on van page

--- components/Vans.jsx
+++ components/Vans.jsx
@@ -9,24 +9,29 @@ export default function Vans(){
         .then(res => res.json())
         .then(data => setVans(data.vans))
     },[])
-
+  
+    const vanElements = vans.map(van=>
+        <div key={van.id} className="van">
+            <img src={van.imageUrl} alt={`van ${van.id}`} className="vanImg"/>
+            <p className="label"><span>{van.name}</span><span>${van.price}</span></p>
+            <span className="pay">/day</span>
+            <span className="van-type type" style={{background:van.type === "simple" ? "#E17654" : (van.type === "rugged" ? "#115E59" : "#161616") }}>{van.type}</span>
+        </div>)
     return (
         <div className="container">
             <Nav />
-            <h3>Explore our van options</h3>
-            <div className="vansFilters">
-                <button>Simple</button>
-                <button>Luxury</button>
-                <button>Rugged</button>
-                <span className="clear">Clear filters</span>
-            </div>
-            <div className="vansCont">
-                <div className="van">
-                    {
-                        vans.map(van=>van.name)
-                    }
+            <div className="vans-body">
+                <h3>Explore our van options</h3>
+                <div className="vansFilters">
+                    <button className="van-type">Simple</button>
+                    <button className="van-type">Luxury</button>
+                    <button className="van-type">Rugged</button>
+                    <span className="clear">Clear filters</span>
                 </div>
-            </div>
+                <div className="vansCont">
+                    { vanElements }
+                </div>
+            </div>  
         </div>
     )
 }

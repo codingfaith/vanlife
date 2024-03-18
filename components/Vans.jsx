c5954241df12e138e18cfc0351d0a9a90b0a4cf7
codingfaith <codingfaith158@gmail.com>
2024-03-18 14:38:58 +0200

    Fetch data from server and import into Vans page. Display block for home button

--- components/Vans.jsx
+++ components/Vans.jsx
@@ -1,10 +1,32 @@
 import React from "react"
 import Nav from "./Nav"
+import "../server"
 
 export default function Vans(){
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+    },[])
+
     return (
         <div className="container">
             <Nav />
+            <h3>Explore our van options</h3>
+            <div className="vansFilters">
+                <button>Simple</button>
+                <button>Luxury</button>
+                <button>Rugged</button>
+                <span className="clear">Clear filters</span>
+            </div>
+            <div className="vansCont">
+                <div className="van">
+                    {
+                        vans.map(van=>van.name)
+                    }
+                </div>
+            </div>
         </div>
     )
 }

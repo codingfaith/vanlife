13e5493501408cafead57647f18c5a2877e2073d
codingfaith <codingfaith158@gmail.com>
2024-03-19 10:28:58 +0200

    Add vanDetail page/component with Nav and Footer

---
+++ components/VanDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Nav from "./Nav"
+import Footer from "./Footer"
+
+export default function VanDetail(){
+    return (
+        <div className="vanDetail-page">
+            <Nav />
+            <Footer />
+        </div>
+    )
+}
